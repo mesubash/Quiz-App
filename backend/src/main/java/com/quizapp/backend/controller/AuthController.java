019bfd647f60bcb52407504554c1ddd238f1c574
mesubash <subashdhamee@gmail.com>
2025-04-03 21:59:59 +0545

    authcontroller added for login and authentiaction preview

---
+++ backend/src/main/java/com/quizapp/backend/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.quizapp.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+
+import com.quizapp.backend.dto.request.AuthRequest;
+import com.quizapp.backend.dto.request.RegisterRequest;
+import com.quizapp.backend.dto.response.AuthResponse;
+import com.quizapp.backend.dto.response.UserResponse;
+import com.quizapp.backend.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> authenticateUser(@Valid @RequestBody AuthRequest authRequest) {
+        return ResponseEntity.ok(authService.authenticateUser(authRequest));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> registerUser(@Valid @RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.registerUser(registerRequest));
+    }
+}
