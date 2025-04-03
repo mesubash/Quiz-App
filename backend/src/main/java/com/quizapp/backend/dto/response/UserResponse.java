8574da5d5957c8f2779fdf32d7452ba04ae027a9
mesubash <subashdhamee@gmail.com>
2025-04-03 21:51:46 +0545

    user response dto added

---
+++ backend/src/main/java/com/quizapp/backend/dto/response/UserResponse.java
@@ -0,0 +1,19 @@
+package com.quizapp.backend.dto.response;
+
+
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
+  
+}
