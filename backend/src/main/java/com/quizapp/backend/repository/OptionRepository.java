ed150608faa6a1d755e7889a96e677ba137c7c9c
mesubash <subashdhamee@gmail.com>
2025-04-07 08:09:20 +0545

    option repo added

--- backend/src/main/java/com/quizapp/backend/repository/OptionRepository.java
+++ backend/src/main/java/com/quizapp/backend/repository/OptionRepository.java
@@ -3,10 +3,11 @@ package com.quizapp.backend.repository;
 import com.quizapp.backend.model.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Long> {
-    List<Option> findByQuestion_Id(Long questionId);
-    List<Option> findByQuestion_IdAndIsCorrectTrue(Long questionId);
+    List<Option> findByQuestionId(Long questionId);
+    List<Option> findByQuestionIdAndIsCorrect(Long questionId, boolean isCorrect);
 }
