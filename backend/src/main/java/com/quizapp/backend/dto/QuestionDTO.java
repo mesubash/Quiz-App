7a8598f37c5ac035abf1a68451a4801cfe7ec1e4
mesubash <subashdhamee@gmail.com>
2025-04-08 02:21:24 +0545

    connected with Quiz and added field like explanation and points

--- backend/src/main/java/com/quizapp/backend/dto/QuestionDTO.java
+++ backend/src/main/java/com/quizapp/backend/dto/QuestionDTO.java
@@ -7,9 +7,10 @@ import lombok.Data;
 
 import java.util.List;
 
-
 import com.quizapp.backend.model.enums.QuestionType;
+import com.quizapp.backend.model.enums.Difficulty;
 
+import jakarta.validation.constraints.NotBlank;
 
 @Data
 @Builder
@@ -17,9 +18,21 @@ import com.quizapp.backend.model.enums.QuestionType;
 @NoArgsConstructor
 public class QuestionDTO {
     private Long id;
+
+    @NotBlank(message = "Question text cannot be blank")
     private String text;
+
+    private String correctAnswer;
+
     private QuestionType questionType;
-    private Integer points;
+
+    private Difficulty difficulty;
+
+    private String explanation;
+
+    private Integer points; //can be used as sum up while user gives the corrent answer in a quiz
+
     private Long quizId;
+
     private List<OptionDTO> options;
 }
