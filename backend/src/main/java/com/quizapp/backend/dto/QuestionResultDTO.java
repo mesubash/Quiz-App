904d8dc333fe59ca8aa56ab67942b5492843b717
mesubash <subashdhamee@gmail.com>
2025-04-07 08:04:35 +0545

    Question Result DTO added

---
+++ backend/src/main/java/com/quizapp/backend/dto/QuestionResultDTO.java
@@ -0,0 +1,15 @@
+package com.quizapp.backend.dto;
+import lombok.Builder;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Builder
+public class QuestionResultDTO {
+    private Long questionId;
+    private String questionText;
+    private boolean correct;
+    private Integer pointsAwarded;
+    private List<Long> correctOptionIds;
+    private List<Long> selectedOptionIds;
+}
